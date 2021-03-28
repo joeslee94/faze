2bf0ebacb823c579fb8c948daba1ce5fdac56684
Joe Lee <75293272+joeslee94@users.noreply.github.com>
2021-03-28 18:45:12 +0900

    Add files via upload

---
+++ Code/FAZE Iteration 1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using FAZE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAZE.Controllers
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult EquipmentRecs()
+        {
+            return View();
+        }
+        public IActionResult ExploreAreas()
+        {
+            return View();
+        }
+        public IActionResult ForgotUsernamePassword()
+        {
+            return View();
+        }
+        public IActionResult Feed()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Code/FAZE Iteration 1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FAZE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
